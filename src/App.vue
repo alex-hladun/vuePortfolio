bc0c6d6bea45f682478b45deff781944eb730264
Alex Hladun <57810269+alex-hladun@users.noreply.github.com>
2020-10-08 03:30:56 -0600

    Styling fixes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <a href="#about"><div :id="logo" class="logo">A/H</div></a>
-  <header :id="menubar" class="menubar">
-    <div class="menu-item" id="aboutNav"><a href="#about">About</a></div>
-    <div class="menu-item" id="projectsNav"><a href="#projects">Projects</a></div>
-    <div class="menu-spacer"></div>
-    <div class="menu-item" id="skillsNav"><a href="#skills">Skills</a></div>
-    <div class="menu-item" id="connectNav"><a href="#connect">Connect</a></div>
+  <header id="menubar" class="menubar">
+    <div class="menu-item" id="menu-about"><a href="#about">About</a></div>
+    <div class="menu-item" id="menu-projects"><a href="#projects">Projects</a></div>
+    <div class="menu-spacer" id="menu-spacer"></div>
+    <div class="menu-item" id="menu-skills"><a href="#skills">Skills</a></div>
+    <div class="menu-item" id="menu-connect"><a href="#connect">Connect</a></div>
     <aside class="scroll-marker"></aside>
   </header>
   <vue-scroll-snap :fullscreen="true">
@@ -42,7 +42,7 @@ import Connect from "./components/Connect.vue";
 import { ref, onMounted, onUnmounted } from "vue";
 import gsap from "./scrollTrigger";
 import VueScrollSnap from "vue-scroll-snap";
-import router from './router'
+import router from "./router";
 
 export default {
   name: "App",
@@ -57,8 +57,8 @@ export default {
   setup() {
     // const home = ref(null);
     console.log("running SETUP");
-    router.push('/#test')
-    console.log('router info', router)
+    router.push("/#test");
+    console.log("router info", router);
 
     onMounted(() => {
       let options = {
@@ -68,27 +68,27 @@ export default {
       };
 
       let obsCallback = (entries, observer) => {
-          console.log("Obserrrrved");
-          // console.log('entries', entries[0])
-          console.log('entries', entries[0].target.id)
-          // console.log('observer', observer)
+        console.log("Obserrrrved");
+        // console.log('entries', entries[0])
+        console.log("entries", entries[0].target.id);
+        // console.log('observer', observer)
 
-          switch(entries[0].target.id) {
-            case 'about':
-            router.push('/#about')
-            break
+        switch (entries[0].target.id) {
+          case "about":
+            router.push("/#about");
+            break;
 
-            case 'projects':
-            router.push('/#projects')
-            break
+          case "projects":
+            router.push("/#projects");
+            break;
 
-            case 'skills':
-            router.push('/#skills')
-            break
-            case 'connect':
-            router.push('/#connect')
-            break
-          }
+          case "skills":
+            router.push("/#skills");
+            break;
+          case "connect":
+            router.push("/#connect");
+            break;
+        }
       };
       let observer = new IntersectionObserver(obsCallback, options);
 
@@ -120,55 +120,71 @@ export default {
         }
       );
 
-      let scrollMarkAnim = gsap.timeline()
+      let scrollMarkAnim = gsap.timeline();
 
-      scrollMarkAnim.to(".scroll-marker", {
-        x:() => '+=$(projectsNav.width)',
-        // xPercent: 20,
-        // ease: "slow",
-        scrollTrigger: {
-          scroller: '.scroll-snap-container',
-          trigger: '.scroll-marker',
-          start: "1400",
-          end:"3000",
-          // duration: 2,
-          // end: "1200",
-          // toggleActions: "play none none reverse",
-          scrub: true,
-          markers: true,
-        },
-      })
-      .to(".scroll-marker", {
-        x:'+=345',
-        immediateRender: false,
-        // xPercent: 20,
-        // ease: "slow",
-        scrollTrigger: {
-          scroller: '.scroll-snap-container',
-          start: "3100",
-          end:"4200",
-          // duration: 2,
-          // end: "1200",
-          // toggleActions: "play none none reverse",
-          scrub: true,
-          markers: true,
-        },
-      })
-      .to(".scroll-marker", {
-        x:'+=160',
-        immediateRender: false,
+     
+      const menubar = document.querySelector("#menubar");
 
-        scrollTrigger: {
-          scroller: '.scroll-snap-container',
-          start: "4300",
-          end:"5200",
-          // duration: 2,
-          // end: "1200",
-          // toggleActions: "play none none reverse",
-          scrub: true,
-          markers: true,
-        },
-      });
+      const menuWidth = menubar.offsetWidth;
+      const totalWidth = document.body.clientWidth;
+      const totalHeight = window.innerHeight;
+      console.log('total height', totalHeight)
+      let start = 1.01*totalHeight;
+      let delta = totalHeight;
+      let spacing = 1;
+
+      const w1 = document.querySelector("#menu-about").offsetWidth;
+      const w2 = document.querySelector("#menu-projects").offsetWidth;
+      const w3 = document.querySelector("#menu-spacer").offsetWidth;
+      const w4 = document.querySelector("#menu-skills").offsetWidth;
+      const w5 = document.querySelector("#menu-connect").offsetWidth;
+
+      const fs = (menuWidth - w1 - w2 - w3 - w4 - w5) / 6;
+
+      console.log(w1, w2, w3, w4, w5, menuWidth);
+      console.log(fs);
+
+      scrollMarkAnim
+        .fromTo(
+          ".scroll-marker",
+          { x: -menuWidth / 2 + fs + w1 / 2 },
+          {
+            x: -menuWidth / 2 + 2 * fs + w1 + w2 / 2,
+            scrollTrigger: {
+              scroller: ".scroll-snap-container",
+              trigger: ".scroll-marker",
+              start: start,
+              end: start + delta,
+              scrub: true,
+              // markers: true,
+            },
+          }
+        )
+        .to(".scroll-marker", {
+          x: -menuWidth / 2 + 4 * fs + w1 + w2 + w3 + w4 / 2,
+          immediateRender: false,
+          scrollTrigger: {
+            scroller: ".scroll-snap-container",
+
+            start: start + delta + spacing,
+            end: start + 2 * delta + spacing,
+            scrub: true,
+            // markers: true,
+          },
+        })
+        .to(".scroll-marker", {
+          x: -menuWidth / 2 + 5 * fs + w1 + w2 + w3 + w4 + w5 / 2,
+          immediateRender: false,
+
+          scrollTrigger: {
+            scroller: ".scroll-snap-container",
+
+            start: start + 2 * delta + 2 * spacing,
+            end: start + 3 * delta,
+            scrub: true,
+            // markers: true,
+          },
+        });
     });
 
     // onUnmounted(() => {
