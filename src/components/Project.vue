ba07432f1fee0692ff445cfa8dcf435eeed713cc
Alex Hladun <57810269+alex-hladun@users.noreply.github.com>
2020-10-08 13:46:54 -0600

    Mobile styling

---
+++ src/components/Project.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <img :class="styles.pic" alt="Alex Hladun" src="../assets/prof2.jpeg" /> -->
+  <div :class="styles.projHeader"><u>{{title}}</u></div>
+  <p :class="styles.projText">
+    {{description}}
+    </p>
+</template>
+
+<script>
+import styles from '../styles/About.module.css'
+
+export default {
+  name: 'Project',
+  props: {
+    title: String,
+    description: String
+  },
+  data() {
+    return {
+      count: 0,
+      styles
+    }
+  }
+}
+</script>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,15 +4,16 @@
   <h1 :class="styles.header"><u>Projects</u></h1>
 
   <!-- <img :class="styles.pic" alt="Alex Hladun" src="../assets/prof2.jpeg" /> -->
-
-  <p :class="styles.text">
-    I am great at lots of projects!
-  </p>
+  <div :class="styles.projHeader"><u>battleShipReact</u></div>
+  <p :class="styles.projText">
+    An online battleship clone. Play against your friends or the computer! Made with React, Redux, TypeScript, Express and Websockets. (Deployed / Git)
+    </p>
   </main>
 </template>
 
 <script>
 import styles from '../styles/About.module.css'
+import Project from './Project.vue'
 
 export default {
   name: 'Projects',
